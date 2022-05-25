16679cb61f2ea085cb39e0e74a4e19644f0c759e
Kaloyan Ivanov <kaloyan.ivanov@gmail.com>
2022-05-25 13:36:48 +0300

    Implemented HomeView component

--- frontend/src/views/HomeView.js
+++ frontend/src/views/HomeView.js
@@ -1,7 +1,21 @@
 import styles from "./HomeView.module.css";
+import Product from "../components/Product";
 
 function HomeView() {
-  return <div className={styles["home-view"]}>HomeView</div>;
+  return (
+    <div className={styles["homeview"]}>
+      <h2 className={styles["homeview-title"]}>Latest Products</h2>
+
+      <div className={styles["homewview-products"]}>
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </div>
+    </div>
+  );
 }
 
 export default HomeView;
