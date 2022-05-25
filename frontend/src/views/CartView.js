097d8a2f4d8f0a099b7580de96b31cfd8b10aa20
Kaloyan Ivanov <kaloyan.ivanov@gmail.com>
2022-05-25 13:34:01 +0300

    Implemented CartView component

--- frontend/src/views/CartView.js
+++ frontend/src/views/CartView.js
@@ -1,7 +1,27 @@
 import styles from "./CartView.module.css";
+import CartItem from "../components/CartItem";
 
 function CartView() {
-  return <div className={styles["cart-view"]}>CartView</div>;
+  return (
+    <div className={styles["cart-view"]}>
+      <div className={styles["cart-left"]}>
+        <h2>Shopping Cart</h2>
+
+        <CartItem />
+      </div>
+
+      <div className={styles["cart-right"]}>
+        <div className={styles["cart-info"]}>
+          <p>Subtotal (0) items</p>
+          <p>$499.99</p>
+        </div>
+
+        <div>
+          <button>Proceed To Checkout</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CartView;
