4ffc65d0a497c46459dc81f41ea798e5a76b909f
Kaloyan Ivanov <kaloyan.ivanov@gmail.com>
2022-05-26 13:03:36 +0300

    Bug fixing
    - bugfix for: Sidebar transition don't work #1
    https://github.com/kaloyan/react-ecommerce-cart/issues/1

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -19,28 +19,26 @@ export default function Sidebar({ show, toggle }) {
   };
 
   return (
-    show && (
-      <div className={classes.join(" ")}>
-        <ul className={styles["sidebar-links"]}>
-          <li>
-            <Link to={"/cart"} onClick={toggle}>
-              <i className="fas fa-shopping-cart"></i>
-              <span>
-                Cart{" "}
-                <span className={styles["sidebar-cart-badge"]}>
-                  {getCartItemsCount()}
-                </span>
-              </span>
-            </Link>
-          </li>
+    <div className={classes.join(" ")}>
+      <ul className={styles["sidebar-links"]}>
+        <li>
+          <Link to={"/"} onClick={toggle}>
+            Shop
+          </Link>
+        </li>
 
-          <li>
-            <Link to={"/"} onClick={toggle}>
-              Shop
-            </Link>
-          </li>
-        </ul>
-      </div>
-    )
+        <li>
+          <Link to={"/cart"} onClick={toggle}>
+            <i className="fas fa-shopping-cart"></i>
+            <span>
+              Cart{" "}
+              <span className={styles["sidebar-cart-badge"]}>
+                {getCartItemsCount()}
+              </span>
+            </span>
+          </Link>
+        </li>
+      </ul>
+    </div>
   );
 }
